3919c533c8b4c203df18b8f74bfd7d9a8ca41438
Rob Passmore <robp@hey.com>
2021-12-02 08:02:09 +0000

    Part 2

--- 2021/day_02/day2.rb
+++ 2021/day_02/day2.rb
@@ -12,7 +12,50 @@ RSpec.describe 'Day 2' do
 
       expect(position(test_input)).to eq(150)
     end
+
+    it 'returns position for actual input' do
+      data = File.open("input.txt").readlines.map(&:chomp)
+      puts position(data)
+    end
   end
+
+  describe 'part 2' do
+    it 'returns a position depth product of 900' do
+      test_input = [
+        'forward 5',
+        'down 5',
+        'forward 8',
+        'up 3',
+        'down 8',
+        'forward 2'
+      ]
+
+      expect(position_v2(test_input)).to eq(900)
+    end
+
+    it 'returns position for actual input' do
+      data = File.open("input.txt").readlines.map(&:chomp)
+      puts position_v2(data)
+    end
+  end
+end
+
+def position_v2(input)
+  map = input.each_with_object({ aim: 0, depth: 0, position: 0 }) do |instruction, h|
+    direction, magnitude = instruction.split(' ')
+    magnitude = magnitude.to_i
+    case direction
+    when 'up'
+      h[:aim] -= magnitude
+    when 'down'
+      h[:aim] += magnitude
+    when 'forward'
+      h[:depth] += h[:aim] * magnitude
+      h[:position] += magnitude
+    end
+  end
+
+  map[:depth] * map[:position]
 end
 
 def position(input)
