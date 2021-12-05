baa6a3d6462463a115771eb3fe913700a91efe4d
Rob Passmore <robp@hey.com>
2021-12-05 19:03:15 +0000

    Day 3 Part II

--- 2021/day_03/day3.rb
+++ 2021/day_03/day3.rb
@@ -1,24 +1,34 @@
 RSpec.describe 'Day 3' do
+  let(:test_input) {
+    %w[
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+    ]
+  }
   describe 'part 1' do
     it 'finds the answer' do
-      test_input = %w[
-        00100
-        11110
-        10110
-        10111
-        10101
-        01111
-        00111
-        11100
-        10000
-        11001
-        00010
-        01010
-      ]
-
       expect(gamma(test_input)).to eq([1, 0, 1, 1, 0])
       expect(epsilon(test_input)).to eq([0, 1, 0, 0, 1])
       expect(power_consumption(test_input)).to eq(198)
+    end
+  end
+
+  describe 'part 2' do
+    it 'finds the answer' do
+      parsed_input = test_input.map { |binary| binary.split('') }
+      expect(o2_gen_rating(parsed_input)).to eq(23)
+      expect(co2_scrub_rating(test_input)).to eq(10)
+      expect(life_support_rating(test_input)).to eq(230)
     end
   end
 end
@@ -45,4 +55,49 @@ def power_consumption(input)
 end
 
 actual_input = File.open("input.txt").readlines.map(&:chomp)
-puts power_consumption(actual_input)
+puts "Part 1: #{ power_consumption(actual_input) }"
+
+# part 2
+
+def o2_gen_rating(input, prefix = '')
+  return prefix.to_i(2) if input.count == 1 && input.first.empty?
+
+  groups = input.each_with_object([[],[]]) do |bin, memo|
+      if bin[0] == '0'
+        memo[0] << bin[1..]
+      else
+        memo[1] << bin[1..]
+      end
+    end
+    if groups[0].count > groups[1].count
+      o2_gen_rating(groups[0], prefix + '0')
+    else
+      o2_gen_rating(groups[1], prefix + '1')
+    end
+end
+
+# basic case: [1, 0, 1] -> [0] -> '0'
+def co2_scrub_rating(input, prefix = '')
+  return prefix.to_i(2) if input.empty? || input.first == ''
+  return (prefix + input.first).to_i(2) if input.count == 1
+
+  groups = input.each_with_object([[],[]]) do |bin, memo|
+      if bin[0] == '0'
+        memo[0] << bin[1..]
+      else
+        memo[1] << bin[1..]
+      end
+  end
+  if groups[0].count <= groups[1].count
+    co2_scrub_rating(groups[0], prefix + '0')
+  else
+    co2_scrub_rating(groups[1], prefix + '1')
+  end
+end
+
+def life_support_rating(input)
+  parsed_input = input.map { |binary| binary.split('') }
+  o2_gen_rating(parsed_input) * co2_scrub_rating(input)
+end
+
+puts "Part II: #{ life_support_rating(actual_input) }"
